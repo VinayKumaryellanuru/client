12012d01aea1c3d38f3025780ace1c5b085c60f4
VinayKumaryellanuru <149552797+VinayKumaryellanuru@users.noreply.github.com>
2024-02-02 15:38:46 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+import Index from './component/Aboutus/Index';
+import './App.css';
+var firstnumber = 10;
+var secondnumber =12;
+function App() {
+  return (
+    <div>
+        <p>vinay</p>
+        documnent.className=getById(sum = ('firstnumber')+('secondnumber'));
+    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
---
+++ src/component/Aboutus/Index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import App from "../../App";
+const Aboutus = () =>{
+   return (<div>
+          <p>vinay</p>    
+    </div>
+   )
+}
+export default Aboutus;
